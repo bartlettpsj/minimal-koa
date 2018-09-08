bb6c437a149872afb570939a5b9c12a1070c024c
Paul Bartlett <paul.bartlett@everyonecounts.com>
2018-09-07 21:13:12 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+// Configuration
+const API_PORT = 3200;
+const API_ENDPOINT = 'api';
+
+const Koa = require('koa');
+const Router = require('koa-router');
+const app = new Koa();
+const router = new Router({ prefix: `/${API_ENDPOINT}`});
+
+router.get('/data/:id', async (ctx, next) => { ctx.body = { greeting: "hello" } });
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(API_PORT);
+
+console.log(`Koa listening at http://localhost:${API_PORT}/${API_ENDPOINT}`);
